cb9ef8e458339ff610fa224d18c3d03bfb88a3e7
Kogoon <42830744+Kogoon@users.noreply.github.com>
2020-01-08 00:04:41 +0900

    200108 before

--- 200107/1463.py
+++ 200107/1463.py
@@ -8,21 +8,24 @@ acmicpc.net/problem/1463
     3. 1을 뺀다.
     정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다.
     연산을 사용하는 횟수의 최솟값을 출력하시오.
+입력: 첫째 줄에 1보다 크거나 같고, 10^6보다 작거나 같은 정수 N이 주어진다.
+출력: 첫째 줄에 연산을 하는 횟수의 최솟값을 출력한다.
 """
 X = int(input())
 count = 0
 while True:
     if(X==1):
         break
-    
-    if(X%3!=0 and X%2!=0):
-        X = X - 1
-        count = count + 1
-    elif(X%3==0):
+
+    if(X%3==0):
         X = X / 3
         count = count + 1
     elif(X%2==0):
         X = X / 2
         count = count + 1
+    elif(X%3!=0 and X%2!=0):
+        X = X - 1
+        count = count + 1
+
 print(count)
 
