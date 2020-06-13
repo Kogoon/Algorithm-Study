3526b335348391f1d94a63e90984f84252df58e9
Kogoon <42830744+Kogoon@users.noreply.github.com>
2020-06-13 23:11:03 +0900

    5086 complete

---
+++ 2006/5086.py
@@ -0,0 +1,16 @@
+# 20.06.13
+"""
+"""
+import sys
+input = sys.stdin.readline
+
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        sys.exit()
+    if b%a==0:
+        print("factor")
+    elif a%b ==0:
+        print("multiple")
+    else:
+        print("neither")
