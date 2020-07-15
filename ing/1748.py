5c72d89315892a23e8cbc625afd26073ac5bad25
JunSung Go <42830744+Kogoon@users.noreply.github.com>
2020-07-16 00:43:48 +0900

    Create 1748.py

---
+++ ing/1748.py
@@ -0,0 +1,16 @@
+#
+"""
+acmicpc.net/problem/1748
+"""
+"""
+# 1 (시간초과)
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+str_num = ''
+for number in range(1, N+1):
+  str_num += str(number)
+
+print(len(str_num))
+"""
