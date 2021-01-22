9238bce380cf413cb366fb5db2ec2fa60f51d85c
JunSung Go <gojs712@gmail.com>
2021-01-22 20:16:12 +0900

    큰 수의 법칙

---
+++ ThisIsCoding/3-2.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline 
+
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+print(first, second)
+
+result = 0 # 결과값. 
+
+while True:
+  for _ in range(k):
+    if m == 0:
+      break
+    result += first
+    m -= 1
+  
+  if m == 0:
+    break
+
+  result += second
+  m -= 1
+
+print(result)
